889d78edadfc7b2d80bf630f7d07889b3435c10f
Christopher Pecoraro <chrispecoraro@gmail.com>
2015-02-07 11:30:00 +0100

    hacking it up

--- src/Orangehill/Iseed/IseedServiceProvider.php
+++ src/Orangehill/Iseed/IseedServiceProvider.php
@@ -36,15 +36,7 @@ class IseedServiceProvider extends ServiceProvider {
     {
 
         // Is it possible to register the config?
-        if (method_exists($this->app['config'], 'package')) {
-            $this->app['config']->package($package, $path.'/config', $namespace);
-        } else {
-            // Load the config for now..
-            $config = $this->app['files']->getRequire($path .'/config/config.php');
-            foreach($config as $key => $value){
-                $this->app['config']->set($namespace.'::'.$key, $value);
-            }
-        }
+       
 
         // Register view files
         $appView = $this->app['path']."/views/packages/{$package}";
